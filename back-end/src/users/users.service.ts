5e8f6b56bc2ce96196cdc49b57f31aa7cae87011
Alireza Khodayi <89078767+Alireza-Khodayi@users.noreply.github.com>
2024-08-20 22:21:13 +0330

    Authentication Back-end implemented

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
@@ -9,7 +9,7 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   private async hashPassword(password: string) {
-    return bcrypt.hash(password!, 10);
+    return bcrypt.hash(password, 10);
   }
 
   async create(createUserInput: CreateUserInput) {
@@ -45,5 +45,16 @@ export class UsersService {
 
   async remove(_id: string) {
     return this.usersRepository.findOneAndDelete({ _id });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+
+    return user;
   }
 }
